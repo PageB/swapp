162654c34f3b019f8c2c174bf6262303435e0439
PageB <PageB88@gmail.com>
2019-10-19 17:18:34 +0300

    Update mock data to the API property names

--- src/components/common/CardCharacter/CardCharacter.js
+++ src/components/common/CardCharacter/CardCharacter.js
@@ -10,9 +10,9 @@ const cardCharacter = props => {
       <img className={styles.CardImage} src={props.card.image} alt={props.card.name} />
       <div className={styles.CardDetails}>
         <CardRowData label="Height: ">{props.card.height}</CardRowData>
-        <CardRowData label="Weight: ">{props.card.weight}</CardRowData>
+        <CardRowData label="Weight: ">{props.card.mass}</CardRowData>
         <CardRowData label="Species: ">{props.card.species}</CardRowData>
-        <CardRowData label="Home World: ">{props.card.world}</CardRowData>
+        <CardRowData label="Home World: ">{props.card.homeworld}</CardRowData>
       </div>
     </div>
   );
--- src/components/common/CardCraft/CardCraft.js
+++ src/components/common/CardCraft/CardCraft.js
@@ -6,23 +6,23 @@ import styles from './CardCraft.module.scss';
 const cardCraft = props => {
   return (
     <div className={[styles.Card, styles[props.theme]].join(' ')}>
-      <p className={styles.CardName}>{props.card.name}</p>
-      <img className={styles.CardImage} src={props.card.image} alt={props.card.name} />
+      <p className={styles.CardName}>{props.starship.name}</p>
+      <img className={styles.CardImage} src={props.starship.image} alt={props.starship.name} />
       <div className={styles.CardDetails}>
         <CardRowData label="Class: " theme={props.theme}>
-          {props.card.class}
+          {props.starship.starshipClass}
         </CardRowData>
         <CardRowData label="Cost: " theme={props.theme}>
-          {props.card.cost}
+          {props.starship.cost}
         </CardRowData>
         <CardRowData label="Crew: " theme={props.theme}>
-          {props.card.crew}
+          {props.starship.crew}
         </CardRowData>
         <CardRowData label="Max Atmospheric speed: " theme={props.theme}>
-          {props.card.speed}
+          {props.starship.maxAtmosphericSpeed}
         </CardRowData>
         <CardRowData label="Hyperdrive Rating: " theme={props.theme}>
-          {props.card.rating}
+          {props.starship.hyperdriveRating}
         </CardRowData>
       </div>
     </div>

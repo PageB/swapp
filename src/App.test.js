5a66806af0becabceedc4e336a422d56b2533d29
PageB <PageB88@gmail.com>
2019-10-22 08:11:01 +0300

    Resolve ESLINT issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Adapter from 'enzyme-adapter-react-16';
-import sinon from 'sinon';
-import { shallow, configure } from 'enzyme';
+import { configure } from 'enzyme';
 import App from './App';
 
 configure({ adapter: new Adapter() });

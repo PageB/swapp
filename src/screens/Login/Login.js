92e50126ff9811ffc406956a4e8562773d3adb32
PageB <PageB88@gmail.com>
2019-10-27 14:27:18 +0200

    Remove ESlint warning

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import ThemeContext from '../../contexts/ThemeContext';
 import LoginForm from '../../components/LoginForm/LoginForm';
 

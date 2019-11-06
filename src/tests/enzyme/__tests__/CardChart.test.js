4b71d61f626e948c9e3e971e539ae7709f5310fe
PageB <PageB88@gmail.com>
2019-11-06 19:36:04 +0200

    Update test of cart chart

--- src/tests/enzyme/__tests__/CardChart.test.js
+++ src/tests/enzyme/__tests__/CardChart.test.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import { starWarsCharacters } from '../../../mock/data';
-import { ApolloProvider } from '@apollo/react-hooks';
 
 import CardChart from '../../../components/CardChart/CardChart';
 
 configure({ adapter: new Adapter() });
 
 describe('CardChart Component', () => {
-  it('should match snapshot with Apollo Provider wrapper', () => {
+  it('should match snapshot', () => {
     const wrapper = shallow(
-      <ApolloProvider>
-        <CardChart starship={starWarsCharacters[0].node.starships[0]} />
-      </ApolloProvider>,
+      <CardChart
+        starship={starWarsCharacters[0].node.starships[0]}
+        starships={starWarsCharacters[0].node.starships}
+      />,
     );
 
     expect(wrapper).toMatchSnapshot();
